2a916f97a93b43cba2cfefc15684e9ba79e95375
statwonk <cpeter9@gmail.com>
2016-10-08 23:34:34 -0500

    Add labels with time units that are easily digestable.

--- plot.R
+++ plot.R
@@ -24,13 +24,39 @@ expand.grid(
   )
 ) %>% tbl_df() %>%
   mutate(how_much_time_you_save_in_one_day = how_often_you_do_the_task * how_much_time_you_shave_off,
-         how_much_time_you_save_in_five_years = how_much_time_you_save_in_one_day*365.25*5 / (60*60*24)) %>%
+         how_much_time_you_save_in_five_years = how_much_time_you_save_in_one_day*365.25*5) %>%
   mutate(how_often_you_do_the_task = factor(how_often_you_do_the_task, levels = unique(how_often_you_do_the_task)),
          how_much_time_you_shave_off = factor(how_much_time_you_shave_off, levels = rev(unique(how_much_time_you_shave_off)))) %>%
   mutate(how_much_time_you_save_in_five_years = ifelse(
-    how_much_time_you_save_in_five_years > 365.25*5,
+    how_much_time_you_save_in_five_years > 60*60*24*365.25*5,
     NA, how_much_time_you_save_in_five_years
   )) %>%
+  filter(!is.na(how_much_time_you_save_in_five_years)) %>%
+  mutate(time_units = cut(
+    how_much_time_you_save_in_five_years,
+    breaks = c(0, 60, 60*60, 60*60*24, 60*60*24*7, 60*60*24*30.5, 60*60*24*365.25, Inf),
+    labels = c("seconds", "minutes", "hours", "days", "weeks", "months", "years"),
+    include.lowest = TRUE, right = FALSE
+  ),
+  time_units = as.character(time_units)) %>%
+  mutate(how_much_time_you_save_in_five_years = Vectorize(function(units_to_adjust_to, time_saved) {
+    switch(units_to_adjust_to,
+           "seconds" = time_saved,
+           "minutes" = time_saved / 60,
+           "hours" = time_saved / (60*60),
+           "days" = time_saved / (60*60*24),
+           "weeks" = time_saved / (60*60*24*7),
+           "months" = time_saved / (60*60*24*30.5),
+           "years" = time_saved / (60*60*24*365.25)
+    )
+  })(time_units, how_much_time_you_save_in_five_years),
+  how_much_time_you_save_in_five_years = round(unlist(how_much_time_you_save_in_five_years), 1)
+  ) %>%
+  # it's 1 second, not 1 seconds ... same for minutes, hours, days ...
+  mutate(time_units = ifelse(how_much_time_you_save_in_five_years == 1,
+                             substr(time_units, 1, nchar(time_units) - 1),
+                             time_units)) %>%
+  mutate(how_much_time_you_save_in_five_years = paste(how_much_time_you_save_in_five_years, time_units)) %>%
   ggplot(aes(x = how_often_you_do_the_task,
              y = how_much_time_you_shave_off
   )) +
